131e53da8ae42606d155b2991194576e34b7d12d
interndpm <intern@dpminter.com>
2024-07-03 10:40:45 +0700

    Add files via upload

---
+++ EKS-TF/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket         = "pp-seoul"
+    region         = "ap-northeast-2"
+    key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
+    dynamodb_table = "pp-seoul-db"
+    encrypt        = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
